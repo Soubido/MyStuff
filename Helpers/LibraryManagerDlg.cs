49fc5399c4c47b4155636456e4efbfb3fef41f52
Soubido <121945551+Soubido@users.noreply.github.com>
2026-02-15 14:27:12 +0100

    Delete Helpers directory

--- Helpers/LibraryManagerDlg.cs
+++
@@ -1,4 +0,0 @@
-// Nur Logik-Snippet:
-// var go = new GetObject(); go.SetCommandPrompt("Select Curve for Library"); ...
-// var name = Rhino.UI.Dialogs.ShowGetString("Profile Name", "Name:", "MyProfile");
-// ProfileLibrary.AddCurve(name, curve);
